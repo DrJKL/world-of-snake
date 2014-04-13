8caaab7272cd1eaa5a8e480cb7cb239a3e28d8c1
Alexander Brown <DrJKL0424@gmail.com>
2014-04-13 16:31:52 -0700

    Attempt to use requirejs with google analytics.

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,6 @@ require.config({
     baseUrl: 'scripts',
     paths: {
         spec: "tests/spec",
-        "google-analytics": [
-            "//www.google-analytics.com/analytics",
-            "vendor/google-analytics" // this is your local copy
-        ],
     },
     shim: {
         "google-analytics": {
