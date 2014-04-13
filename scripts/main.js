62ffdae06fb7c091da75a3a6288f5a2a10becca2
Alexander Brown <DrJKL0424@gmail.com>
2014-04-13 11:29:30 -0700

    Start modifying main.js to allow for ga events tracking.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 require.config({
     baseUrl: 'scripts',
     paths: {
-        spec: "tests/spec"
+        spec: "tests/spec",
+        "google-analytics": [
+            "//www.google-analytics.com/analytics",
+            "vendor/google-analytics" // this is your local copy
+        ],
+    },
+    shim: {
+        "google-analytics": {
+            exports: "ga",
+        },
     }
 });
